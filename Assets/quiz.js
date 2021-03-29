58c4eae2f5065b1e43bf565a1d6931b33453faec
tristan <mousy93@hotmail.com>
2021-03-29 12:14:32 +0800

    wrote some psuedo code
    starting writing out some pseudo code to get a plan started

---
+++ Assets/quiz.js
@@ -0,0 +1,29 @@
+/* psuedo code: 
+i must make a multi choice quick 
+-layout buttons on screen for answers if correct change color to green if wrong change color 
+to red 
+-hover over button make slightly opaic to highlight choice
+-build a timer if timer reaches 0 quiz ends if wrong answer subtract from time
+-when start button is clicked timer starts
+-when timer ends show field to enter initials in and save to local to keep highscore
+-when a question is answerd correctly must disply next question
+-after initials has be saved have a highscore and go back button 
+
+-write psuedo for each section eg timer, answers, end screen
+
+quiz buttons:
+-butons on hover highlight or go opaic
+-button on correct answer change background of button to green 
+-if correct changes to next question
+-button on wrong answer changes btn background to red and stays on the same screen
+
+Timer:
+-
+
+End screen:
+-
+
+Highscore button/screen:
+-
+
+*/

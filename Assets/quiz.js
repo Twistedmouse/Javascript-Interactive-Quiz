d18f3cd4e64c3aa3b14f0a009fd827609f931cda
tristan <mousy93@hotmail.com>
2021-04-02 16:12:46 +0800

    started js and chaged data atributes in quiz.html to id
    changed data attributes to id for easier javascript

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -38,141 +38,119 @@ High score button/screen:
 const QUESTION = document.querySelector("#question");
 const CHOICES = Array.from(document.querySelectorAll(".choice-text"));
 const progressTEXT = document.querySelector("#progressText");
-const scoreText = document.querySelector("#score");
 const progressBarFull = document.querySelector("#progressBarFull");
+const scoreText = document.querySelector("#score");
+const MAX_QUIESTIONS = 8;
 
-let currentQuestion = {}
-let acceptingAnswers = true
+let TotalScore = scoreText;
+let currentQuestion = {};
+let acceptingAnswers = true;
 // score is my timer
-let score = 60
-let questionCounter = 0
-let availableQuestions = []
+let score = 60;
+let questionCounter = 0;
+let availableQuestion = [];
 
 // an array that is basically the whole quiz
 let questions = [
     {
-        question: "Inside which HTML element do we put the javascript?",
-        choice1: "<js>",
-        choice2: "<scripting>",
-        choice3: "<javascript>",
-        choice4: "<script>",
-        answer: 3,
+        title: "Inside which HTML element do we put the javascript?",
+        choices: [
+            "<js>", "<scripting>", "<javascript>", "<script>"
+        ],
+        answer: "<script>"
     },
     {
-        question: "Where is the correct place to insert a JavaScript?",
-        choice1: "The <head> section",
-        choice2: "Both the <head> section and the <body> section are correct",
-        choice3: "The <body> section",
-        choice4: "The <footer> section",
-        answer: 2,
+        title: "Where is the correct place to insert a JavaScript?",
+        choices:[
+            "The <head> section", "Both the <head> section and the <body> section are correct",
+            "The <body> section", "The <footer> section"
+        ],
+        answer: "Both the <head> section and the <body> section are correct"
     },
     {
-        question: "What is the correct syntax for referring to an external script called "xxx.js"?",
-        choice1: "<script src="xxx.js">",
-        choice2: "<script href="xxx.js">",
-        choice3: "<script name="xxx.js">",
-        choice4: "<script link="xxx.js">",
-        answer: 1,
+        title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        choices:[
+            '<script src="xxx.js">', '<script href="xxx.js">', '<script name="xxx.js">', '<script link="xxx.js">'
+        ],
+        answer: '<script src="xxx.js">'
     },
     {
-        question: "How do you write "Hello World" in an alert box?",
-        choice1: "msg("Hello World");",
-        choice2: "msgBox("Hello World");",
-        choice3: "alertBox("Hello World");",
-        choice4: "alert("Hello World");",
-        answer: 4,
+        title: 'How do you write "Hello World" in an alert box?',
+        choices:[
+            'msg("Hello World");', 'msgBox("Hello World");', 'alertBox("Hello World");', 'alert("Hello World");'
+        ],
+        answer: 'alert("Hello World");'
     },
     {
-        question: "How do you create a function in JavaScript?",
-        choice1: "function = myFunction()",
-        choice2: "function:myFunction()",
-        choice3: "function myFunction()",
-        choice4: "function, myFunction:",
-        answer: 3,
+        title: "How do you create a function in JavaScript?",
+        choices:[
+            "function = myFunction()", "function:myFunction()", "function myFunction()", "function, myFunction:"
+        ],
+        answer: "function myFunction()"
     },
     {
-        question: "How do you call a function named "myFunction"?",
-        choice1: "myFunction()",
-        choice2: "call myFunction()",
-        choice3: "call function myFunction()",
-        choice4: "ringRing pick up the myFunction()",
-        answer: 1,
+        title: 'How do you call a function named "myFunction"?',
+        choices:[ 
+            "myFunction()", "call myFunction()", "call function myFunction()", "ringRing pick up the myFunction()"
+        ],
+        answer: "myFunction()"
     },
     {
-        question: "How to write an IF statement in JavaScript?",
-        choice1: "if (i == 5)",
-        choice2: "if i == 5 then",
-        choice3: "if i = 5",
-        choice4: "if i = 5 then",
-        answer: 1,
+        title: "How to write an IF statement in JavaScript?",
+        choices:[
+            "if (i == 5)", "if i == 5 then", "if i = 5", "if i = 5 then"
+        ],
+        answer: "if (i == 5)"
     },
     {
-        question: "How to write an IF statement for executing some code if "i" is NOT equal to 5?",
-        choice1: "if i =! 5 then",
-        choice2: "if i <> 5",
-        choice3: "if (i <> 5)",
-        choice4: "if (i != 5) ",
-        answer: 4,
+        title: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+        choices:[
+            "if i =! 5 then", "if i <> 5", "if (i <> 5)", "if (i != 5) "
+        ],
+        answer: "if (i != 5) "
     },
     {
-        question: "How does a WHILE loop start?",
-        choice1: "while i = 1 to 10",
-        choice2: "while (i <= 10) ",
-        choice3: "while (i <= 10; i++)",
-        choice4: "while: i = 1 <= 10 + 1",
-        answer: 2,
+        title: "How does a WHILE loop start?",
+        choices:[
+            "while i = 1 to 10", "while (i <= 10) ", "while (i <= 10; i++)", "while: i = 1 <= 10 + 1"
+        ],
+        answer: "while (i <= 10)"
     },
     {
-        question: "How does a FOR loop start?",
-        choice1: "for (i = 0; i <= 5)",
-        choice2: "for (i = 0; i <= 5; i++)  ",
-        choice3: "for i = 1 to 5",
-        choice4: "for (i <= 5; i++)",
-        answer: 2,
+        title: "How does a FOR loop start?",
+        choices:[
+            "for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)"
+        ],
+        answer: "for (i = 0; i <= 5; i++)"
     },
     {
-        question: "How can you add a comment in a JavaScript?",
-        choice1: "<!--This is a comment-->",
-        choice2: "-this is a comment-",
-        choice3: "//This is a comment  ",
-        choice4: "'This is a comment",
-        answer: 3,
-    },
+        title: "How can you add a comment in a JavaScript?",
+        choices:[
+            "<!--This is a comment-->", "-this is a comment-", "//This is a comment  ", "This is a comment"
+        ],
+        answer: "//This is a comment"
+    }
 ]
 
-const SCORE_POINTS = 
-const MAX_QUIESTIONS = 4
-
-startGame = () => {
+startGame() 
+//funtion to reset my start game
+function startGame() {
     questionCounter = 0
     score = 60
-    availableQuestions = [...questions]
     getNewQuestion()
 }
-// function to save scores to the local storage and handles the questions
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUIESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-
-        return window.location.assign("end.html")
-    }
-    // ` ${}` a way to concatenate strings
-    questionCounter++
-    progressTEXT.innerText = `Question ${questionCounter} of ${MAX_QUIESTIONS}`
-    // shows length of progress bar based on percentage
-    progressBarFull.style.width = `${(questionCounter/MAX_QUIESTIONS) * 100}%`
-// randomises questions
-    const questionIndex = math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex]
-    question.innerText = currentQuestion.question
-// how how app knows what choices we're making
-    CHOICES.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion["choice" + number]
-    })
-// spice adds and removes items from the array
-    availableQuestions.splice(questionIndex, 1)
-
-    acceptingAnswers = true
+//function to get the question to display 
+function getNewQuestion() {
+    let titleElement = document.querySelector("#questionTitle")
+    let questionObject = questions[questionCounter]
+    let titleText = questionObject.title
+    titleElement.textContent = titleText
+    answerChoices()
 }
+
+function answerChoices() {
+    let choicesElement = document.querySelector(".choiceText")
+    let choiceObjects = 
+}
+
 
